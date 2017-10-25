15810ebf45965cae26f8bbf1e13c40145e9c27e0
jackson.demaree <jackson.demaree@tamu.edu>
2017-10-25 11:28:40 -0400

    eureka and zuul

--- src/main/java/com/revature/HystrixDashboardDemoApplication.java
+++ src/main/java/com/revature/HystrixDashboardDemoApplication.java
@@ -2,10 +2,14 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableTurbine
+@EnableDiscoveryClient
 public class HystrixDashboardDemoApplication {
 
 	public static void main(String[] args) {
